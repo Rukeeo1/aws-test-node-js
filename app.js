2096a646022a6892aac0d39ee0dfa15a79cd0069
rukeeo1 <rukeeojigbo@gmail.com>
2023-05-23 21:28:35 +0200

    chore: add dummy data

--- app.js
+++ app.js
@@ -1,8 +1,125 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+
+const shoes = [
+  {
+    id: 1,
+    name: 'Running Shoes',
+    brand: 'Nike',
+    price: 59.99,
+    image: 'running_shoes.jpg',
+    description:
+      'These running shoes provide excellent cushioning and support.',
+    category: 'Sports',
+    shoeCategory: 'Running',
+    rating: 4.5,
+    reviews: 10,
+    availability: true,
+  },
+  {
+    id: 2,
+    name: 'Sneakers',
+    brand: 'Adidas',
+    price: 49.99,
+    image: 'sneakers.jpg',
+    description: 'Casual and comfortable sneakers for everyday wear.',
+    category: 'Fashion',
+    shoeCategory: 'Casual',
+    rating: 4.2,
+    reviews: 8,
+    availability: true,
+  },
+  {
+    id: 3,
+    name: 'High Heels',
+    brand: 'Gucci',
+    price: 79.99,
+    image: 'high_heels.jpg',
+    description: 'Elegant high heels that will make you stand out.',
+    category: 'Fashion',
+    shoeCategory: 'Formal',
+    rating: 4.7,
+    reviews: 15,
+    availability: true,
+  },
+  {
+    id: 4,
+    name: 'Basketball Shoes',
+    brand: 'Air Jordan',
+    price: 89.99,
+    image: 'basketball_shoes.jpg',
+    description: 'Designed for optimal performance on the basketball court.',
+    category: 'Sports',
+    shoeCategory: 'Basketball',
+    rating: 4.8,
+    reviews: 12,
+    availability: false,
+  },
+  // Add more shoe varieties here...
+];
+
+const jsonData = JSON.stringify(shoes);
+
+// Write the JSON data to a file
+fs.writeFile('shoes.json', jsonData, 'utf8', (err) => {
+  if (err) {
+    console.error('Error writing JSON file:', err);
+    return;
+  }
+  console.log('JSON file has been saved successfully.');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello, Rukees experess');
+});
+
+app.get('/shoes', (req, res) => {
+  // Read the JSON file
+  fs.readFile('shoes.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading JSON file:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    const shoesData = JSON.parse(data);
+    res.json(shoesData);
+  });
+});
+
+app.post('/shoes', (req, res) => {
+  const newShoe = req.body;
+
+  // Read the JSON file
+  fs.readFile('shoes.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading JSON file:', err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    const shoesData = JSON.parse(data);
+
+    // Generate a new ID for the shoe
+    const newId = shoesData.length + 1;
+    newShoe.id = newId;
+
+    // Add the new shoe to the array
+    shoesData.push(newShoe);
+
+    // Convert the updated array to JSON
+    const updatedJsonData = JSON.stringify(shoesData);
+
+    // Write the updated JSON data to the file
+    fs.writeFile('shoes.json', updatedJsonData, 'utf8', (err) => {
+      if (err) {
+        console.error('Error writing JSON file:', err);
+        res.status(500).send('Internal Server Error');
+        return;
+      }
+      res.json({ message: 'Shoe added successfully.', shoe: newShoe });
+    });
+  });
 });
 
 // Start the server
